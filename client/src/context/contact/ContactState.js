a4b1f11db3ec8ac08b6f768806da1b2a0db89c86
joyson <jng05929@gmail.com>
2020-04-06 22:02:21 +0530

    added context

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -33,21 +33,31 @@ const ContactState = props => {
         name: 'Harry White',
         email: 'harry@gmail.com',
         phone: '333-333-3333',
-        type: 'personal'
+        type: 'professional'
       }
-    ]
+    ],
+    current: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //Add contact
-
+  const addContact = contact => {
+    contact.id = uuid;
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   //Delete contact
-
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   //Set current contact
-
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   //Clear current contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   //Update contact
 
   //Filter contact
@@ -57,7 +67,12 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent
       }}
     >
       {props.children}
