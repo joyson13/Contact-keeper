dba7e4b3fb8d6a7a0fbbce0b35a01192fe7ca5ec
joyson <jng05929@gmail.com>
2020-04-07 14:00:04 +0530

    added login and rgister routes

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
         type: 'professional'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -59,20 +60,31 @@ const ContactState = props => {
     dispatch({ type: CLEAR_CURRENT });
   };
   //Update contact
-
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   //Filter contact
-
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   //Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
