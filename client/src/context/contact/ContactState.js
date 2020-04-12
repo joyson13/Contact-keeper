7c5727d2312ff7d036cf733b670662d12cd5914e
joyson <jng05929@gmail.com>
2020-04-12 21:56:22 +0530

    added edit user

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,6 +63,27 @@ const ContactState = (props) => {
     }
   };
 
+  //Update contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   //Clear contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -76,10 +97,7 @@ const ContactState = (props) => {
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
-  //Update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
-  };
+
   //Filter contact
   const filterContacts = (text) => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
